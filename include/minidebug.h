6c4a8df55157fd945deebe23483f3c188264c75a
ididnotknowhwatotuseforausername <59734049+ididnotknowhwatotuseforausername@users.noreply.github.com>
2020-01-10 08:02:58 -0500

    Update minidebug.h

--- include/minidebug.h
+++ include/minidebug.h
@@ -1,1 +1,20 @@
+#ifndef __MINIDEBUG
+#define __MINIDEBUG
 
+#include <stdio.h> // string formatting
+
+// Parse an absolute path into a filename such as MyFile.cpp
+const char* __GetBaseFileName(
+    const char* File_Macro
+);
+
+// A more conventional way to get the source file line this func is called on
+unsigned int __GetCurrentLine(
+    unsigned int Line
+);
+
+void DebugCout(
+    const char* Message,
+    const char* FIleName,
+    unsigned int Line
+);
